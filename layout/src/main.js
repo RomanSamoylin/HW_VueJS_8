4ed2be91581868b5d5f515a422ebd970de5eedfc
MacBook <macbook@MacBook-Pro-MacBook.local>
2024-07-08 16:22:11 +0300

    router

--- layout/src/main.js
+++ layout/src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 import store from './store'
 import VueCarousel from 'vue-carousel'
 
@@ -8,6 +9,7 @@ Vue.use(VueCarousel)
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   store,
   render: h => h(App)
 }).$mount('#app')
